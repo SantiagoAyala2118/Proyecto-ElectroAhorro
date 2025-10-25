c4023228ca5272f48288fd0b92053ee051d8901d
Santiagio Ayala <santitomayala07@gmail.com>
2025-10-25 17:40:40 -0300

    Ahora se importa App como pagina principal de home

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,11 +2,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Registro } from "../pages/Registro.jsx";
 import { Login } from "../pages/Login.jsx";
-import { Home } from "../pages/Home.jsx";
+import { App } from "../App.jsx";
 
 // Se hace una constante router en donde se guardan todas las direcciones para ir navegando por los componentes
 export const router = createBrowserRouter([
   { path: "/", element: <Registro /> },
   { path: "/login", element: <Login /> },
-  { path: "/app", element: <Home /> },
+  { path: "/app", element: <App /> },
 ]);

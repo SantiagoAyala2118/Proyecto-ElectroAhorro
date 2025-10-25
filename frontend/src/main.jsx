960143601d7bac3cf69c392bf35b2989ae431ac0
Santiagio Ayala <santitomayala07@gmail.com>
2025-10-25 09:24:10 -0300

    Se quitó el if else para la renderización

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index.jsx";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
 
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
-  );
-} else {
-  // En desarrollo, si no existe #root esto evita que la app lance un error crasheando todo.
-  // Podés quitar este else si preferís que falle explícitamente para detectar el problema.
-  console.error("Root element not found");
-}
+createRoot(rootElement).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);

8d7fbe4fd48b2277289d9c9c224b49a14535e2de
NatZigaran. <natanaelzigaran@gmail.com>
2025-10-29 19:46:10 -0300

    Se creo la Page de calculadora

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -3,10 +3,12 @@ import { createBrowserRouter } from "react-router-dom";
 import { Registro } from "../pages/Registro.jsx";
 import { Login } from "../pages/Login.jsx";
 import { App } from "../App.jsx";
+import Calculadora from "../pages/Calculadora.jsx";
 
 // Se hace una constante router en donde se guardan todas las direcciones para ir navegando por los componentes
 export const router = createBrowserRouter([
   { path: "/", element: <Registro /> },
   { path: "/login", element: <Login /> },
   { path: "/app", element: <App /> },
+  { path: "/calculator", element: <Calculadora /> },
 ]);

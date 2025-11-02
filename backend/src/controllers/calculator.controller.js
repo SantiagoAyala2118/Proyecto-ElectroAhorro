205829db5ddb0008bc01795a5f5424051785a60f
Santiagio Ayala <santitomayala07@gmail.com>
2025-11-02 19:50:36 -0300

    Se corrigieron algunos campos para evitar errores en la lógica de ejecución

--- backend/src/controllers/calculator.controller.js
+++ backend/src/controllers/calculator.controller.js
@@ -29,12 +29,17 @@ export const createCalculation = async (req, res) => {
     });
 
     // ! ESTO ES PARA QUE EL FRONT LEA LA DATA Y GENERE LOS GRÁFICOS
-    const perDayKwh = (power * hours_per_day) / 1000;
+    const perDayKwh =
+      (validatedData.power * validatedData.hours_per_day) / 1000;
+
     const dailyData = [];
+
     const today = new Date();
-    for (let i = 0; i < days; i++) {
+
+    for (let i = 0; i < validatedData.days; i++) {
       const d = new Date(today);
       d.setDate(today.getDate() + i);
+
       dailyData.push({
         date: d.toISOString().slice(0, 10), // "YYYY-MM-DD"
         kwh: Number(perDayKwh.toFixed(4)),

59edabe07be08d1036b1d3686a5ce79228ddff6e
Santiagio Ayala <santitomayala07@gmail.com>
2025-11-05 12:19:04 -0300

    Ahora lo que se renderiza es el AppRouter con las rutas privadas y publicas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { Home } from "./pages/Home.jsx";
 import { Hero } from "./components/layouts/Hero.jsx";
+import { AppRouter } from "./routes/AppRouter.jsx";
 
 export const App = () => {
   return (
     <div className="min-w-full">
-      <Home />
-      <Hero />
+      <AppRouter />
     </div>
   );
 };

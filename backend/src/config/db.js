5494c307d03cbb2387eb2b2e87dd48596e134a21
Santiagio Ayala <santitomayala07@gmail.com>
2025-11-20 09:55:35 -0300

    Actualiza la sincronización de la base de datos y corrige la clave de API en el entorno

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,7 @@ export const startDB = async () => {
     console.log("Conexión exitosa a la Base de datos");
 
     // Cambiar temporalmente para recrear tablas
-    await sequelize.sync();
+    await sequelize.sync({ force: false });
     console.log("Sincronización exitosa con la Base de datos");
   } catch (err) {
     console.error("Error al conectar a la base de datos:", err);
